ad265f678218adf3dfceecdb7d182b7f4120b237
nahnahAJ <nahnahahjuwah@gmail.com>
2022-10-26 16:15:30 +0000

    Added styles to the Calculator app

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../index.css';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -10,35 +11,40 @@ class Calculator extends React.Component {
   populateFunc = () => {
     const numbers = [];
     for (let i = 1; i < 10; i += 1) {
-      numbers.push(<button type="button">{i}</button>);
+      numbers.push(
+        <button type="button" key={i}>
+          {i}
+        </button>,
+      );
     }
     return numbers;
-  }
+  };
 
   render() {
     return (
       <div className="mainUI">
-        <span>0</span>
+        <div className="calculator">
+          <div className="display">
+            <span>0</span>
+          </div>
+          <div className="grid">
+            <div className="row1">
+              <button type="button">AC</button>
+              <button type="button">+/-</button>
+              <button type="button">%</button>
+              {this.populateFunc()}
+              <button type="button" className="zero">0</button>
+              <button type="button">.</button>
+            </div>
 
-        <div className="row1">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-        </div>
-
-        <div className="column4">
-          <button type="button">÷</button>
-          <button type="button">x</button>
-          <button type="button">-</button>
-          <button type="button">+</button>
-          <button type="button">=</button>
-        </div>
-
-        <div className="numbers">{this.populateFunc()}</div>
-
-        <div className="row5">
-          <button type="button">0</button>
-          <button type="button">.</button>
+            <div className="column4">
+              <button type="button">÷</button>
+              <button type="button">x</button>
+              <button type="button">-</button>
+              <button type="button">+</button>
+              <button type="button">=</button>
+            </div>
+          </div>
         </div>
       </div>
     );
