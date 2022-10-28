6450c6b89dd587517bcaf91092819f12b4991fca
nahnahAJ <nahnahahjuwah@gmail.com>
2022-10-28 10:09:26 +0000

    Refactored code from class components to functional components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,59 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css';
-
-const populateFunc = () => {
-  const numbers = [];
-  for (let i = 1; i < 10; i += 1) {
-    numbers.push(
-      <button name={i} type="button" key={i}>
-        {i}
-      </button>,
-    );
-  }
-  return numbers;
-};
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [state, addState] = useState({ total: 0, next: null, operation: null });
+
+  const addLogic = (e) => {
+    addState((state) => calculate(state, e.target.name));
+  };
+
+  const populateFunc = () => {
+    const numbers = [];
+    for (let i = 1; i < 10; i += 1) {
+      numbers.push(
+        <button name={i} type="button" key={i} onClick={addLogic}>
+          {i}
+        </button>,
+      );
+    }
+    return numbers;
+  };
+
   return (
     <div className="mainUI">
       <div className="calculator">
         <div className="display">
-          <span>0</span>
+          <span>{ state.total }</span>
+          <span>{ state.operation }</span>
+          <span>{ state.next }</span>
         </div>
         <div className="grid">
           <div className="row1">
-            <button name="AC" type="button">
+            <button name="AC" type="button" onClick={addLogic}>
               AC
             </button>
-            <button name="+/-" type="button">
+            <button name="+/-" type="button" onClick={addLogic}>
               +/-
             </button>
-            <button name="%" type="button">
+            <button name="%" type="button" onClick={addLogic}>
               %
             </button>
             {populateFunc()}
-            <button name="0" type="button" className="zero">
+            <button name="0" type="button" className="zero" onClick={addLogic}>
               0
             </button>
-            <button name="." type="button">
+            <button name="." type="button" onClick={addLogic}>
               .
             </button>
           </div>
 
           <div className="column4">
-            <button name="÷" type="button">
+            <button name="÷" type="button" onClick={addLogic}>
               ÷
             </button>
-            <button name="x" type="button">
+            <button name="x" type="button" onClick={addLogic}>
               x
             </button>
-            <button name="-" type="button">
+            <button name="-" type="button" onClick={addLogic}>
               -
             </button>
-            <button name="+" type="button">
+            <button name="+" type="button" onClick={addLogic}>
               +
             </button>
-            <button name="=" type="button">
+            <button name="=" type="button" onClick={addLogic}>
               =
             </button>
           </div>
