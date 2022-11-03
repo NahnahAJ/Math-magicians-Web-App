0dbe10891accde3c8d0ce84cb1a4532222db4d60
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-03 08:35:55 +0000

    Added code for the quote page

---
+++ src/pages/QuotePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './QuotePage.css';
+
+const QuotePage = () => (
+  <div className="quotes">
+    Mathematics is not about numbers, equations, computations, or algorithms:
+    It is about understanding.
+    <p>~William Paul Thurston</p>
+  </div>
+);
+
+export default QuotePage;
